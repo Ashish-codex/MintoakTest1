44426873e0c8f51f69584b38cad4e862b5286edd
Ashish Prajapati <88182121+Ashish-codex@users.noreply.github.com>
2025-10-17 12:35:47 +0530

    Initial Commit

---
+++ MintoakTest1/MintoakTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  MintoakTest1App.swift
+//  MintoakTest1
+//
+//  Created by Ashish Prajapati on 17/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MintoakTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
